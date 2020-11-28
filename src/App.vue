668d07c6cda9ff196f837e7625c7fd1d4398f771
JuliaZamaitat <j.zamaitat@gmail.com>
2020-11-28 18:10:11 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Diashows />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Diashows from './components/Diashows.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Diashows
   }
 }
 </script>
